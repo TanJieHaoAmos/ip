90f5708a23100de7c3b556dcf35dc48c93b4e3b8
TanJieHaoAmos <156202208+TanJieHaoAmos@users.noreply.github.com>
2025-01-18 16:42:44 +0800

    Update main class

--- src/main/java/AmosTalky.java
+++ src/main/java/AmosTalky.java
@@ -1,9 +1,12 @@
 public class AmosTalky {
     static String name = "AmosTalky";
+    static String line = "-----------------------\n";
 
     public static void main(String[] args) {
-
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
+        System.out.print(line);
+        System.out.println("Hello! I'm "
+                + name + "\nWhat can I do for you?\n"
+                + line + "Bye. Hope to see you again soon!\n"
+                + line);
     }
 }

735d9ed3b69bdce2b1d6cd5719bab2c1ec7fde61
TanJieHaoAmos <156202208+TanJieHaoAmos@users.noreply.github.com>
2025-01-18 16:35:15 +0800

    Rename main class

---
+++ src/main/java/AmosTalky.java
@@ -0,0 +1,9 @@
+public class AmosTalky {
+    static String name = "AmosTalky";
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+    }
+}

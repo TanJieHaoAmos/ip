8398652c805485dfb67c098e1899bab03455889c
TanJieHaoAmos <156202208+TanJieHaoAmos@users.noreply.github.com>
2025-01-20 21:40:56 +0800

    Complete A-TextUiTesting Rename Main.java

--- src/main/java/Runny.java
+++ src/main/java/Runny.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Runny {
     public static void main(String[] args) {
         AmosTalky chat = new AmosTalky();
         Scanner scanner = new Scanner(System.in);
